e97bff12f92943dc8be07ee3e1dbaa7a40b25c7e
Esteban538 <sosazapata0503@gmail.com>
2025-05-01 19:32:47 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+<footer class="pie">
+    <div class="content">
+        <div class="col">
+            <h5>DIRECCION:</h5>
+            <p>Almirante Grau</p>
+            <h5>TELEFONO:</h5>
+            <p>987654321</p>
+            <h5>CORREO:</h5>
+            <p>estebansosa@gmail.com</p>
+        </div>
+        <div class="col">
+            <h5>QUIENES SOMOS</h5>
+            <a href="#">INICIO</a>
+            <a href="#">CONTACTANOS</a>
+            <a href="#">CARRERAS</a>
+            <a href="#">PROMOCIONES</a>
+        </div>
+        <div class="col">
+            <h5>DIRECCION:</h5>
+            <p>Almirante Grau</p>
+            <h5>TELEFONO:</h5>
+            <p>987654321</p>
+            <h5>CORREO:</h5>
+            <p>estebansosa@gmail.com</p>
+        </div>
+        <div class="iconos">
+            <a href="https://web.facebook.com/iesappiura/?locale=es_LA&_rdc=1&_rdr" target="_self"><i
+                    class="fab fa-facebook"></i></a>
+            <a href="https://www.tiktok.com/@institutoalasperuanas" target="_self"><i class="fab fa-tiktok"></i></a>
+            <a href="https://www.instagram.com/institutoalasperuanas/" target="_self"><i
+                    class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+    <div class="copy">
+        <small>&copy; 2025 El Beber Me Llama. Todos los derechos reservados.</small>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include('includes/header.php');?>
+<div class="contenedor">
+    <div class="descripcion">
+        <h2>Frugos</h2>
+        <p>Los frugos son bebidas no alcohólicas elaboradas a base de jugo de frutas, listas para consumir. Son
+            refrescantes, prácticos y se presentan en diversos sabores, ideales para niños y adultos.
+        </p>
+    </div>
+    <center><table class="tablaproductos">
+        <tr>
+            <td>
+                <img src="img/frugos/cifrut350ml.png" height="250px"><br>
+                FRESH VALLE DE NARANJA – 500ML<br>
+                <strong>S/2.50</strong>
+            </td>
+            <td>
+                <img src="img/frugos/freshvallecaja1l.png" height="250px"><br>
+                FRESH VALLE DE NARANJA – 500ML<br>
+                <strong>S/2.50</strong>
+            </td>
+            <td>
+                <img src="img/frugos/freshvalle3.png" height="250px"><br>
+                FRESH VALLE DE NARANJA – 500ML<br>
+                <strong>S/2.50</strong>
+            </td>
+        </tr>
+    </table><center>
+</div>
+
+<?php include('includes/footer.php');?>
